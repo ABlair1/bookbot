c743127cffa3dc2ddba1bdcad3210d58529eb8bb
Andrew Blair <andrewjblair@gmail.com>
2025-06-18 17:00:21 -0400

    Add function to get sorted counts and refactor. Format output

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-from stats import get_char_counts, get_word_count
+import sys
+from stats import get_char_counts, get_sorted_char_list, get_word_count
 
 
-filepath = "./books/frankenstein.txt"
+def validate_input():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-def get_book_text(filepath):
+def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         text = f.read()
         return text
+    
+def get_book_report(
+        filepath: str,
+        word_count: int,
+        sorted_char_counts: list[dict]
+    ) -> str:
+    report = (
+        f"============ BOOKBOT ============\n"
+        f"Analyzing book found at {filepath}...\n"
+        f"----------- Word Count ----------\n"
+        f"Found {word_count} total words\n"
+        f"--------- Character Count -------\n"
+    )
+    for count in sorted_char_counts:
+        if count["char"].isalpha():
+            report += f"{count['char']}: {count['num']}\n"
+    report += "============= END ==============="
+    return report
+
 
 def main():
+    validate_input()
+    filepath = sys.argv[1]
+
     text = get_book_text(filepath)
-
     word_count = get_word_count(text)
-    print(f"{word_count} words found in the document")
+    char_counts = get_char_counts(text)
+    sorted_char_counts = get_sorted_char_list(char_counts)
+    
+    print(get_book_report(filepath, word_count, sorted_char_counts))
 
-    character_counts = get_char_counts(text)
-    print(character_counts)
 
-
-main()
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -1,8 +1,7 @@
-def get_word_count(text):
-    word_count = len(text.split())
-    return word_count
+def get_word_count(text: str) -> int:
+    return len(text.split())
 
-def get_char_counts(text):
+def get_char_counts(text: str) -> dict:
     char_counts = {}
     for char in text:
         if char.lower() in char_counts:
@@ -10,3 +9,8 @@ def get_char_counts(text):
         else:
             char_counts[char.lower()] = 1
     return char_counts
+
+def get_sorted_char_list(char_counts: dict) -> list[dict]:
+    counts_list = [{"char": c, "num": n} for c, n in char_counts.items()]
+    counts_list.sort(reverse=True, key=lambda count: count["num"])
+    return counts_list
